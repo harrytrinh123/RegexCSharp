1c840016735373e03f071d06e8ebdbf342cda46a
jacktrinh97 <hoangtq.iuh@gmail.com>
2020-12-19 11:05:58 +0700

    first commit

---
+++ demoRegex/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace demoRegex
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.regex = new System.Windows.Forms.TextBox();
+            this.stringInput = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.labelResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // regex
+            // 
+            this.regex.Location = new System.Drawing.Point(231, 84);
+            this.regex.Name = "regex";
+            this.regex.Size = new System.Drawing.Size(305, 31);
+            this.regex.TabIndex = 1;
+            this.regex.Text = "<.*?>";
+            // 
+            // stringInput
+            // 
+            this.stringInput.Location = new System.Drawing.Point(231, 141);
+            this.stringInput.Name = "stringInput";
+            this.stringInput.Size = new System.Drawing.Size(305, 31);
+            this.stringInput.TabIndex = 2;
+            this.stringInput.KeyUp += new System.Windows.Forms.KeyEventHandler(this.stringInput_KeyUp);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(121, 84);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 25);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Pattern";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(121, 141);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Text";
+            // 
+            // labelResult
+            // 
+            this.labelResult.AutoSize = true;
+            this.labelResult.Location = new System.Drawing.Point(398, 209);
+            this.labelResult.Name = "labelResult";
+            this.labelResult.Size = new System.Drawing.Size(0, 25);
+            this.labelResult.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(683, 306);
+            this.Controls.Add(this.labelResult);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.stringInput);
+            this.Controls.Add(this.regex);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox regex;
+        private System.Windows.Forms.TextBox stringInput;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label labelResult;
+    }
+}
+
---
+++ demoRegex/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace demoRegex
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void stringInput_KeyUp(object sender, KeyEventArgs e)
+        {
+            Regex reg = new Regex(regex.Text);
+            bool result = reg.IsMatch(stringInput.Text);
+            labelResult.Text = result.ToString();
+        }
+    }
+}
